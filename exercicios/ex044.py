62320d2ed63cfd65f245451aed67a4d9a5ba81fa
Ricardo Ferreira Leão <leao_xingu@outlook.com.br>
2025-02-11 12:04:53 -0300

    ate exercício 45

--- exercicios/ex044.py
+++ exercicios/ex044.py
@@ -1,4 +1,4 @@
-preco = float(input('Qual o valor do produto: R$ '))
+compra = float(input('Qual o valor do produto: R$ '))
 print('')
 print('-=-' * 15)
 print('        CONDIÇÕES DE PAGAMENTO')
@@ -10,4 +10,19 @@ print('''[ 1 ] À VISTA DINHEIRO / CHEQUE: 10% DE DESCONTO!
 [ 4 ] 3X NO CARTAO OU MAIS: 20% DE JUROS!''')
 print('')
 cond = int(input('Escolha a forma de pagamento: '))
-
+if cond == 1:
+    total = compra - (compra * 10 / 100)
+elif cond == 2:
+    total = compra - (compra * 5 / 100)
+elif cond == 3:
+    total = compra
+    print('Sua compra sera dividida em 2x de {:.2f}'.format(compra / 2))
+elif cond == 4:
+    total = compra + (compra * 20 / 100)
+    parc = int(input('Em quantas vezes sera dividida a compra: '))
+    valpar = total / parc
+    print('Sua compra sera divivida em {}X de R${:.2f}'.format(parc, valpar))
+else:
+    print('opção invalida')
+    total = compra
+print('O valor da sua compra é R$ {:.2f} e o valor total a pagar é R${:.2f}'.format(compra, total))
---
+++ exercicios/ex045.py
@@ -0,0 +1,12 @@
+from random import randint
+comp = randint(0,2)
+itens = ('pedra', 'papel', 'tesoura')
+print('Famos jogar JO, KEN, PO:')
+print('-=' * 15)
+print('''[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+print('-=' * 15)
+jog = int(input('Escolha uma opção: '))
+
+
